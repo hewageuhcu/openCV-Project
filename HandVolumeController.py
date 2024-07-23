09907813cf32626f00fed078eca8556dc855d6b3
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-07-24 04:45:25 +0900

    corrected some errors

--- HandVolumeController.py
+++ HandVolumeController.py
@@ -4,27 +4,25 @@ import numpy as np
 import HandTrackingModule as htm
 
 #########################################
-wCam,hCam=640,480
+wCam, hCam = 640, 480
 #########################################
 
-cap=cv2.VideoCapture(0)
-cap.set(3,wCam)
-cap.set(4,hCam)
-pTime=0
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
 
-detector=htm.handDetector()
-
+detector = htm.handDetector()
 
 while True:
-    success,img=cap.read()
-    img=detector.findHands(img)
-    
-    
-    cTime=time.time()
-    fps=1/(cTime-pTime)
-    pTime=cTime
-    
-    cv2.putText(img,f'FPS: {int(fps)}',(40,70),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,255),3)
-    
-    cv2.imshow("Img",img)
+    success, img = cap.read()
+    img = detector.findHands(img)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 3)
+
+    cv2.imshow("Img", img)
     cv2.waitKey(1)
