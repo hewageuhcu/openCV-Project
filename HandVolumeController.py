26be4f9379518d4441c91f6ba66ef5a399bfa4be
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-07-25 03:53:39 +0900

    updated

--- HandVolumeController.py
+++ HandVolumeController.py
@@ -20,8 +20,14 @@ while True:
     lmList=detector.findPosition(img,draw=False)
     if len(lmList)!=0:
         {
-    print(lmList[2])
+    print(lmList[4],lmList[8])
 }
+        x1,y1=lmList[4][1],lmList[4][2]
+        x2,y2=lmList[8][1],lmList[8][2]
+        
+        cv2.circle(img,(x1,y1),15,(255,0,255),cv2.FILLED)
+   
+   
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
