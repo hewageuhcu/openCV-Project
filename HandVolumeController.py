bf37b01cd6e1deec786a2dff0184a0be54f465b2
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-08-25 22:28:02 +0900

    updated

--- HandVolumeController.py
+++ HandVolumeController.py
@@ -63,11 +63,10 @@ while True:
                                 
                       
         if length<50:  
-              7
               cv2.circle(img,(cx,cy),15,(0,255,0),cv2.FILLED)  
             
     cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
-    cv2.rectangle(img,(50,int(vol)),(85,400),(0,255,0),cv2.FILLED)
+    cv2.rectangle(img,(50,int(volBar)),(85,400),(0,255,0),cv2.FILLED)
     
     cTime = time.time()
     fps = 1 / (cTime - pTime)
